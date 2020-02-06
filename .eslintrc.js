d37384ef76b3b5f7e1e5160fabb070d0965e944c
sberney <sam@samuelberney.com>
2020-02-05 22:32:28 -0800

    Install eslint, and add script to execute it using "npm run lint" or "yarn lint". Currently, linter is upset as lack of imports.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint-config-i-am-meticulous",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
