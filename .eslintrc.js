3feba47fc19e986bdaf986eb5defe2bbc940840e
sberney <sam@samuelberney.com>
2020-02-05 22:50:57 -0800

    Can add globals here, until converted.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
+const libraryGlobals = {
+  "d3": "readonly"
+};
+
 module.exports = {
   "env": {
     "browser": true,
     "es6": true
   },
   "extends": "eslint-config-i-am-meticulous",
-  "globals": {
+  "globals": Object.assign({
     "Atomics": "readonly",
     "SharedArrayBuffer": "readonly"
-  },
+  }, libraryGlobals),
   "parserOptions": {
     "ecmaVersion": 2018,
     "sourceType": "module"
